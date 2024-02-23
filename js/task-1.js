b6bae5d92525dcfc8f7c06760ee3280ecd342ed4
Eraserstreet <148534901+Eraserstreet@users.noreply.github.com>
2024-02-23 17:30:44 +0200

    Home-JS-7-0.2

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,12 @@
+const categoriesList = document.querySelector('#categories')
 
+const categoriesElements = categoriesList.querySelectorAll('li.item')
 
+categoriesElements.forEach(categoryElement => {
+	const categoryTitle = categoryElement.querySelector('h2').innerHTML
+	const categoryElements = categoryElement.querySelectorAll('ul li')
+	console.log(`Category: ${categoryTitle}`)
+	console.log(`Elements: ${categoryElements.length}`)
+})
 
-
-
-
-
-
-
-
-
+console.log('Number of categories:', categoriesElements.length)
