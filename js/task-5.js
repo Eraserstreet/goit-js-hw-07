db8861e48fb26ac332e02f1fecce353a64f39ac3
Eraserstreet <148534901+Eraserstreet@users.noreply.github.com>
2024-02-24 13:09:23 +0200

    Home-JS-7-0.7

---
+++ js/task-5.js
@@ -0,0 +1,17 @@
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`
+}
+
+const buttonChangeColor = document.querySelector('.change-color')
+const bodyElement = document.body
+const colorSpan = document.querySelector('.color')
+
+buttonChangeColor.addEventListener('click', function () {
+	const randomColor = getRandomHexColor()
+
+	bodyElement.style.backgroundColor = randomColor
+
+	colorSpan.textContent = randomColor
+})
